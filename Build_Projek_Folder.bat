b65ec94e2f9c1c584499030592ba5f40c16637b4
kuhyakuya <kuhyakuya@gmail.com>
2025-03-20 13:43:22 +0700

    Update Build_Projek_Folder.bat

--- Build_Projek_Folder.bat
+++ Build_Projek_Folder.bat
@@ -17,12 +17,12 @@ if not exist "%hidden_dummy_path%" (
 attrib +h "%hidden_dummy_path%" >nul 2>&1
 
 :: === PINDAHKAN FILE DUMMY KE FOLDER TERSEMBUNYI (REPLACE JIKA ADA) ===
-if exist "9681a60a-9dc4-45d7-9837-6aeae46facaf.dat" move /Y "9681a60a-9dc4-45d7-9837-6aeae46facaf.dat" "%hidden_dummy_path%\9681a60a-9dc4-45d7-9837-6aeae46facaf.dat"
-if exist "261740ad-ad29-48d7-9102-0f3e0e02d716.dat" move /Y "261740ad-ad29-48d7-9102-0f3e0e02d716.dat" "%hidden_dummy_path%\261740ad-ad29-48d7-9102-0f3e0e02d716.dat"
+if exist "9681a60a-9dc4-45d7-9837-6aeae46facaf.aam" move /Y "9681a60a-9dc4-45d7-9837-6aeae46facaf.aam" "%hidden_dummy_path%\9681a60a-9dc4-45d7-9837-6aeae46facaf.aam"
+if exist "261740ad-ad29-48d7-9102-0f3e0e02d716.aam" move /Y "261740ad-ad29-48d7-9102-0f3e0e02d716.aam" "%hidden_dummy_path%\261740ad-ad29-48d7-9102-0f3e0e02d716.aam"
 
 :: === SHOW FILE DUMMY SEBELUM COPY ===
-attrib -h "%hidden_dummy_path%\9681a60a-9dc4-45d7-9837-6aeae46facaf.dat" >nul 2>&1
-attrib -h "%hidden_dummy_path%\261740ad-ad29-48d7-9102-0f3e0e02d716.dat" >nul 2>&1
+attrib -h "%hidden_dummy_path%\9681a60a-9dc4-45d7-9837-6aeae46facaf.aam" >nul 2>&1
+attrib -h "%hidden_dummy_path%\261740ad-ad29-48d7-9102-0f3e0e02d716.aam" >nul 2>&1
 
 :: === BUAT STRUKTUR FOLDER PROYEK ===
 mkdir "%project_path%"
@@ -43,12 +43,12 @@ mkdir "%project_path%\Spine\Revisi"
 mkdir "%project_path%\Spine\Tambahan"
 
 :: === COPY FILE DUMMY KE FOLDER PROYEK DAN RENAME ===
-copy /Y "%hidden_dummy_path%\9681a60a-9dc4-45d7-9837-6aeae46facaf.dat" "%project_path%\Adobe Files\%project_name%.prproj"
-copy /Y "%hidden_dummy_path%\261740ad-ad29-48d7-9102-0f3e0e02d716.dat" "%project_path%\Adobe Files\%project_name%.aep"
+copy /Y "%hidden_dummy_path%\9681a60a-9dc4-45d7-9837-6aeae46facaf.aam" "%project_path%\Adobe Files\%project_name%.prproj"
+copy /Y "%hidden_dummy_path%\261740ad-ad29-48d7-9102-0f3e0e02d716.aam" "%project_path%\Adobe Files\%project_name%.aep"
 
 :: === SEMBUNYIKAN KEMBALI FILE DUMMY DI D:\DummyHidden ===
-attrib +h "%hidden_dummy_path%\9681a60a-9dc4-45d7-9837-6aeae46facaf.dat" >nul 2>&1
-attrib +h "%hidden_dummy_path%\261740ad-ad29-48d7-9102-0f3e0e02d716.dat" >nul 2>&1
+attrib +h "%hidden_dummy_path%\9681a60a-9dc4-45d7-9837-6aeae46facaf.aam" >nul 2>&1
+attrib +h "%hidden_dummy_path%\261740ad-ad29-48d7-9102-0f3e0e02d716.aam" >nul 2>&1
 
 :: === BUAT SHORTCUT KE PROJECT & FILE DUMMY ===
 if not exist "%shortcut_path%" mkdir "%shortcut_path%"
